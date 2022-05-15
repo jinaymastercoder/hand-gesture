b5eb55a9202f9ad44826d834cbdd13e0f7c6a4f4
jinaymastercoder <105051460+jinaymastercoder@users.noreply.github.com>
2022-05-15 17:00:35 +0530

    Add files via upload

---
+++ hand gesture/main.js
@@ -0,0 +1,87 @@
+Webcam.set({
+    width :350,
+    height :300,
+    image_format : 'png',
+    png_quality :90
+ });
+ 
+ 
+ camera = document.getElementById("camera");
+ 
+ 
+ Webcam.attach('#camera');
+ 
+ 
+ function take_snapshot(){
+ 
+     Webcam.snap(function(data_uri){
+     document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+     });
+ }
+ 
+ console.log('ml5 version', ml5.version);
+ 
+ classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/CUV7nLDi5/model.json', modelLoaded);
+ 
+ 
+ function modelLoaded(){
+     console.log('Model Is Loaded!')
+ }
+ 
+ 
+ function speak(){
+     var synth = window.speechSynthesis;
+     speak_data_1 = "The First Prediction Is -" + prediction_1;
+     speak_data_2 = "And The Second Prediction Is -" +prediction_2;
+     var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+     synth.speak(utterThis);
+ }
+ 
+ 
+ function check()
+ {
+     img = document.getElementById('captured_image');
+     classifier.classify(img, gotResults);
+ }
+ 
+ 
+ function gotResults( error , results){
+ 
+     if(error){
+     console.error(error);
+     }
+     else{
+         console.log(results);
+         document.getElementById("result_hand_name").innerHTML = results[0].label;
+         document.getElementById("result_hand_name2").innerHTML = results[1].label;
+         prediction_1 = results[0].label;
+         prediction_2 = results[1].label;
+         speak();
+ 
+         if(results[0].label == "clap")
+         {
+             document.getElementById("update_emoji").innerHTML = "&#128075;";
+         }
+         if(results[0].label == "perfect")
+         {
+             document.getElementById("update_emoji").innerHTML = "&#128076;";
+         }
+         if(results[0].label == "thumbs-up")
+         {
+             document.getElementById("update_emoji").innerHTML = "&#128077;";
+         }
+         if(results[1].label == "clap")
+         {
+             document.getElementById("update_emoji2").innerHTML = "&#128075;";
+         }
+         if(results[1].label == "perfect")
+         {
+             document.getElementById("update_emoji2").innerHTML = "&#128076;";
+         }
+         if(results[1].label == "thumbs-up")
+         {
+             document.getElementById("update_emoji2").innerHTML = "&#128077;";
+         }
+     }
+ }
+ 
